59dd13b8535aa70ce6fd57350fc57584061b5f92
unknown <115050813@qq.com>
2024-05-19 21:46:40 +0800

    feat: add imgui

--- app/src/main/java/io/github/jmecn/font/editor/Main.java
+++ app/src/main/java/io/github/jmecn/font/editor/Main.java
@@ -4,8 +4,6 @@ import com.jme3.app.DetailedProfilerState;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.StatsAppState;
 import com.jme3.app.state.AppState;
-import com.jme3.material.MaterialDef;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Spatial;
@@ -13,16 +11,21 @@ import com.jme3.system.AppSettings;
 import com.jme3.texture.Texture;
 import com.jme3.util.SkyFactory;
 import imgui.*;
+import imgui.type.ImBoolean;
+import imgui.type.ImFloat;
+import imgui.type.ImInt;
+import imgui.type.ImString;
 import io.github.jmecn.font.CommonChars;
-import io.github.jmecn.font.FtBitmapCharacterSet;
 import io.github.jmecn.font.editor.app.CheckerBoardState;
 import io.github.jmecn.font.editor.app.LightState;
-import io.github.jmecn.font.generator.FtFontGenerator;
 import io.github.jmecn.font.generator.FtFontParameter;
 import io.github.jmecn.font.generator.enums.Hinting;
 import io.github.jmecn.font.generator.enums.RenderMode;
-import io.github.jmecn.font.packer.Packer;
+import io.github.jmecn.font.packer.strategy.GuillotineStrategy;
+import io.github.jmecn.font.packer.strategy.SkylineStrategy;
 import io.github.jmecn.font.plugins.FtFontLoader;
+
+import java.util.Arrays;
 
 /**
  * desc:
@@ -43,10 +46,121 @@ public class Main extends SimpleApplication {
 
     private final FtFontParameter parameter;
 
+    // here is all the FtFontParameters, for imgui
+    ImInt size = new ImInt();
+    ImInt renderMode = new ImInt();
+    String[] renderModes;
+
+    ImInt spread = new ImInt();
+    ImInt hinting = new ImInt();
+    String[] hintings;
+    ImBoolean kerning = new ImBoolean();
+    ImBoolean incremental = new ImBoolean();
+    ImString characters = new ImString();
+
+    // packer
+    ImInt packerSize = new ImInt();
+    ImInt packPadding = new ImInt();
+    ImInt packStrategy = new ImInt();// 0 - SkylineStrategy, 1 -
+    String[] strategyOptions;
+
+    // color
+    float[] color = new float[4];// rgba
+    ImFloat gamma = new ImFloat();
+    ImInt renderCount = new ImInt();
+
+    // border
+    ImFloat borderWidth = new ImFloat();
+    float[] borderColor = new float[4];
+    ImBoolean borderStraight = new ImBoolean();
+    ImFloat borderGamma = new ImFloat();
+    // shadow
+    ImInt shadowOffsetX = new ImInt();
+    ImInt shadowOffsetY = new ImInt();
+    float[] shadowColor = new float[4];
+    // spacing
+    ImInt spaceX = new ImInt();
+    ImInt spaceY = new ImInt();
+
+    // padding
+    ImInt padTop = new ImInt();
+    ImInt padLeft = new ImInt();
+    ImInt padBottom = new ImInt();
+    ImInt padRight = new ImInt();
+
+    // texture
+    ImBoolean genMipMaps = new ImBoolean();
+    ImInt minFilter = new ImInt();
+    String[] minFilterOptions;
+    ImInt magFilter = new ImInt();
+    String[] magFilterOptions;
+
+    // materials
+    ImString matDefName = new ImString();
+    ImString colorMapParamName = new ImString();
+    ImString vertexColorParamName = new ImString();
+    ImBoolean useVertexColor = new ImBoolean();
+
     public Main(AppState... states) {
         super(states);
         parameter = new FtFontParameter();
+
+        packerSize.set(1024);
+        packPadding.set(1);
+        packStrategy.set(0);
+
+        renderModes = Arrays.stream(RenderMode.values()).map(RenderMode::name).toArray(String[]::new);
+        hintings = Arrays.stream(Hinting.values()).map(Hinting::name).toArray(String[]::new);
+        strategyOptions = new String[] {GuillotineStrategy.class.getSimpleName(), SkylineStrategy.class.getSimpleName()};
+        minFilterOptions = Arrays.stream(Texture.MinFilter.values()).map(Texture.MinFilter::name).toArray(String[]::new);
+        magFilterOptions = Arrays.stream(Texture.MagFilter.values()).map(Texture.MagFilter::name).toArray(String[]::new);
     }
+
+    /**
+     * set parameter to imgui
+     * @param params
+     */
+    private void setParameter(FtFontParameter params) {
+        size.set(params.getSize());
+        renderMode.set(params.getRenderMode().ordinal());
+        spread.set(params.getSpread());
+        hinting.set(params.getHinting().ordinal());
+        kerning.set(params.isKerning());
+        incremental.set(params.isIncremental());
+
+        characters.set(params.getCharacters());
+
+        color = params.getColor().toArray(color);
+        gamma.set(params.getGamma());
+        renderCount.set(params.getRenderCount());
+
+        borderWidth.set(params.getBorderWidth());
+        borderColor = params.getBorderColor().toArray(borderColor);
+        borderStraight.set(params.isBorderStraight());
+        borderGamma.set(params.getBorderGamma());
+
+        shadowOffsetX.set(params.getShadowOffsetX());
+        shadowOffsetY.set(params.getShadowOffsetY());
+        shadowColor = params.getShadowColor().toArray(shadowColor);
+
+        spaceX.set(params.getSpaceX());
+        spaceY.set(params.getSpaceY());
+
+        padTop.set(params.getPadTop());
+        padLeft.set(params.getPadLeft());
+        padBottom.set(params.getPadBottom());
+        padRight.set(params.getPadRight());
+
+        genMipMaps.set(params.isGenMipMaps());
+        minFilter.set(params.getMinFilter().ordinal());
+        magFilter.set(params.getMagFilter().ordinal());
+
+        matDefName.set(params.getMatDefName());
+        colorMapParamName.set(params.getColorMapParamName());
+        vertexColorParamName.set(params.getVertexColorParamName());
+        useVertexColor.set(params.isUseVertexColor());
+    }
+
 
     @Override
     public void simpleInitApp() {
@@ -68,6 +182,7 @@ public class Main extends SimpleApplication {
 
         // init imgui
         initImGui();
+        setParameter(parameter);
 
         ///// init app state /////
         stateManager.attach(new LightState());
@@ -124,47 +239,55 @@ public class Main extends SimpleApplication {
         // Start the ImGui frame
         ImGuiJme3.startFrame();
 
-        ImGui.text("你好，世界");
+        // menubar
+
+        ImGui.begin("Packer");
+        ImGui.inputInt("size", packerSize);
+        ImGui.inputInt("padding", packPadding);
+        ImGui.combo("strategy", packStrategy, strategyOptions);
+        ImGui.end();
+
+        ImGui.begin("FtFontParameters");
+
+
+        ImGui.inputInt("size", size);
+        ImGui.combo("renderMode", renderMode, renderModes);
+
+        ImGui.colorEdit4("color", color);
+        ImGui.inputFloat("gamma", gamma);
+        ImGui.inputInt("renderCount", renderCount);
+        ImGui.inputInt("spread", spread);
+        ImGui.combo("hinting", hinting, hintings);
+        ImGui.checkbox("kerning", kerning);
+        ImGui.checkbox("incremental", incremental);
 
         // 将 parameter 中的参数全部使用 imgui 绘制出来
-        /*
-        private int size = 16;
-        private RenderMode renderMode = RenderMode.NORMAL;
-        private int spread = 2;
-        private Hinting hinting = Hinting.NORMAL;
-        private ColorRGBA color = ColorRGBA.White;
-        private float gamma = 1.8f;
-        private int renderCount = 2;
-        private float borderWidth = 0;
-        private ColorRGBA borderColor = ColorRGBA.Black;
-        private boolean borderStraight = false;
-        private float borderGamma = 1.8f;
-        private int shadowOffsetX = 0;
-        private int shadowOffsetY = 0;
-        private ColorRGBA shadowColor = new ColorRGBA(0, 0, 0, 0.75f);
-        private int spaceX;
-        private int spaceY;
-        private int padTop;
-        private int padLeft;
-        private int padBottom;
-        private int padRight;
-        private String characters = CommonChars.ASCII.getChars();
-        private boolean kerning = true;
-        private Packer packer = null;
-        private boolean genMipMaps = false;
-        private Texture.MinFilter minFilter = Texture.MinFilter.NearestNoMipMaps;
-        private Texture.MagFilter magFilter = Texture.MagFilter.Bilinear;
-        private MaterialDef matDef;
-        private String matDefName = "Common/MatDefs/Misc/Unshaded.j3md";
-        private String colorMapParamName = "ColorMap";// or DiffuseMap in Lighting.j3md
-        private boolean useVertexColor = true;
-        private String vertexColorParamName = "VertexColor";
-        private boolean incremental = false;
-        */
 
+        ImGui.inputFloat("borerWidth", borderWidth);
+        ImGui.colorEdit4("borderColor", borderColor);
+        ImGui.checkbox("borderStraight", borderStraight);
+        ImGui.inputFloat("borderGamma", borderGamma);
 
-        ImGui.checkbox("动态生成", parameter.isIncremental());
+        ImGui.inputInt("shadowOffsetX", shadowOffsetX);
+        ImGui.inputInt("shadowOffsetY", shadowOffsetY);
+        ImGui.colorEdit4("shadowColor", shadowColor);
 
+        ImGui.inputInt("spaceX", spaceX);
+        ImGui.inputInt("spaceY", spaceY);
+
+        ImGui.inputInt("padLeft", padLeft);
+        ImGui.inputInt("padRight", padRight);
+        ImGui.inputInt("padTop", padTop);
+        ImGui.inputInt("padBottom", padBottom);
+
+        ImGui.checkbox("genMipMaps", genMipMaps);
+        ImGui.combo("minFilter", minFilter, minFilterOptions);
+        ImGui.combo("magFilter", magFilter, magFilterOptions);
+
+        if (ImGui.button("Generate")) {
+            parameter.setIncremental(incremental.get());
+        }
+        ImGui.end();
 
         // End the ImGui frame
         ImGuiJme3.endFrame();
